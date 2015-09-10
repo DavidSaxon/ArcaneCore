176f0b4eea1961bb0400f08338ae954394e6a9d7
David Saxon <davidesaxon@gmail.com>
2015-09-11 00:12:16 +1200

    relative paths for doxygen

--- docs/build/html/search/all_1.js
+++ docs/build/html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['utf8string',['UTF8String',['../classchaos_1_1base_1_1str_1_1_u_t_f8_string.html',1,'chaos::base::str']]],
-  ['utf8string',['UTF8String',['../classchaos_1_1base_1_1str_1_1_u_t_f8_string.html#a17f6acafbdc6a6733bc00d12f3dac688',1,'chaos::base::str::UTF8String']]]
+  ['utf8string',['UTF8String',['../classchaos_1_1base_1_1str_1_1_u_t_f8_string.html#a404c2331943a5698381cb88fcbe07e02',1,'chaos::base::str::UTF8String']]]
 ];
---
+++ docs/build/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cu",
+  1: "u",
+  2: "c",
+  3: "u"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions"
+};
+
