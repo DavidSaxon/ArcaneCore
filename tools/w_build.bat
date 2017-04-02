a29d93ad0c9e47fc5ba9793dbb0e21761a599704
David Saxon <davidesaxon@gmail.com>
2017-04-02 23:36:34 +1200

    updated for windows 10

--- tools/w_build.bat
+++ tools/w_build.bat
@@ -1,5 +1,3 @@
-set VCTargetsPath=c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
-
 msbuild ..\ArcaneCore.sln /v:n /p:Configuration=Base_lib
 msbuild ..\ArcaneCore.sln /v:n /p:Configuration=IO_lib
 msbuild ..\ArcaneCore.sln /v:n /p:Configuration=GFX_lib
